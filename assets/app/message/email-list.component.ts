bf091e371841bcfe3a9c5e965e9a70ee3032808e
unknown <daniel.caligiuri44@gmail.com>
2017-10-13 21:15:03 -0400

    try again

--- assets/app/message/email-list.component.ts
+++ assets/app/message/email-list.component.ts
@@ -42,17 +42,17 @@ public currentPath: string;
                 }
             );
 
-        this.sub = this.route.params.subscribe(params => {
-        this.searchTerm = params['searchTerm']; 
-        this.emailService.searchMessages(this.searchTerm)
-                    .subscribe(
-                        (messages: Email[]) => {
-                            this.messages = messages;
-                        }
-                    );
+        //this.sub = this.route.params.subscribe(params => {
+        //this.searchTerm = params['searchTerm']; 
+        //this.emailService.searchMessages(this.searchTerm)
+        //            .subscribe(
+        //                (messages: Email[]) => {
+        ///                    this.messages = messages;
+        //                }
+        ///            );
         
 
-    });
+    ///});
 
         this.subscription = this.emailService.upp$
             .subscribe(item => this.upper = item);
