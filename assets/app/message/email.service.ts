dc47ad543b94833874b5592f0cfcd00b26d4bde4
unknown <daniel.caligiuri44@gmail.com>
2017-10-15 18:36:49 -0400

    try again

--- assets/app/message/email.service.ts
+++ assets/app/message/email.service.ts
@@ -277,7 +277,6 @@ export class EmailService {
         const headers = new Headers({'Content-Type': 'application/json'});
         return this.http.post('https://dansgmail.herokuapp.com/mail/markAsReadHighlighted' + token + modifiedTarget, highlighted, {headers: headers})
             .map((response: Response) => {
-                /*
                 const messages = response.json().obj;
                 let transformedMessages: Email[] = [];
                 for (let message of messages) {
@@ -301,8 +300,7 @@ export class EmailService {
                 this.emails = transformedMessages.sort(function(a, b) {
                     return (a.timeStamp < b.timeStamp) ? -1 : ((a.timeStamp > b.timeStamp) ? 1 : 0);
                 });
-                //return transformedMessages.reverse();
-                */
+                return transformedMessages.reverse();
             })
             .catch((error: Response) => Observable.throw(error.json()));
     }
@@ -322,7 +320,6 @@ export class EmailService {
         const headers = new Headers({'Content-Type': 'application/json'});
         return this.http.post('https://dansgmail.herokuapp.com/mail/markAsUnreadHighlighted' + token + modifiedTarget, highlighted, {headers: headers})
             .map((response: Response) => {
-                /*
                 const messages = response.json().obj;
                 let transformedMessages: Email[] = [];
                 for (let message of messages) {
@@ -346,8 +343,7 @@ export class EmailService {
                 this.emails = transformedMessages.sort(function(a, b) {
                     return (a.timeStamp < b.timeStamp) ? -1 : ((a.timeStamp > b.timeStamp) ? 1 : 0);
                 });
-                //return transformedMessages.reverse();
-                */
+                return transformedMessages.reverse();
             })
             .catch((error: Response) => Observable.throw(error.json()));
     }
@@ -532,7 +528,7 @@ export class EmailService {
                     return (a.timeStamp < b.timeStamp) ? -1 : ((a.timeStamp > b.timeStamp) ? 1 : 0);
                 });
 
-                //return transformedMessages.reverse();
+                return transformedMessages.reverse();
             })
             //.catch((error: Response) => Observable.throw(error.json()));
     }
