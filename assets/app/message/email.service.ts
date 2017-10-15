00b8047c4479cd9d7d48b609c14afdf2df8a18b5
unknown <daniel.caligiuri44@gmail.com>
2017-10-15 12:22:36 -0400

    try again

--- assets/app/message/email.service.ts
+++ assets/app/message/email.service.ts
@@ -493,7 +493,7 @@ export class EmailService {
                     this.unreadEmails = transformedMessages.reverse().reverse();
                 }
                 this.emails = transformedMessages.reverse();
-                //return transformedMessages.reverse();
+                return transformedMessages.reverse();
             })
             //.catch((error: Response) => Observable.throw(error.json()));
     }
