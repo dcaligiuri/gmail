475885bebb4793e56dd18ad75b6e46cf1f9e5a63
unknown <daniel.caligiuri44@gmail.com>
2017-10-18 19:53:29 -0400

    try again

--- routes/message.js
+++ routes/message.js
@@ -433,8 +433,7 @@ router.post('/markAsReadHighlighted', function (req, res, next) {
     console.log(arrToRead);
 
 
-
-    Email.update( {_id : {"$in":arrToRead}}, {active:false} , {multi: true} 
+    Email.update( { _id: { $in: arrToRead } }, {active:false} , {multi: true} 
         , function(err,docs) { console.log(docs); });
 
 
