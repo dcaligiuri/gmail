8884a72f5268d5a768f63978d7a063ade740fa40
unknown <daniel.caligiuri44@gmail.com>
2017-10-21 18:32:53 -0400

    changes

--- routes/message.js
+++ routes/message.js
@@ -66,7 +66,7 @@ router.post('/moveEmail', function (req, res, next) {
           console.log(req.query.newLocation);
 
 
-          Email.update( { _id: { $in: arrIdToMove } }, {$addToSet: { "labels": req.query.newLocation }} , {multi: true} 
+          Email.update( { _id: { $in: arrIdToMove } }, {$addToSet: { "labels": req.query.newLocation }, $pull: { "labels": req.query.oldLocation } } , {multi: true} 
         , function(err,docs) 
         { 
             Email.find(queryCodes[req.query.oldLocation])
