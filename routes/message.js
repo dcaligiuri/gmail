0300cca948ac676053c6662edfe1cf4e57b2b6b8
unknown <daniel.caligiuri44@gmail.com>
2017-10-22 17:45:58 -0400

    changes

--- routes/message.js
+++ routes/message.js
@@ -242,6 +242,7 @@ router.get('/search/:searchTerm', function (req, res, next) {
 
 
 router.post('/primary', function (req, res, next) {
+    console.log(req.body.auth);
     var decoded = jwt.decode(req.body.auth);
     Email.find({ "user": decoded.user._id, "spam": "false", "trash":"false", "labels" : { $in: [ "primary" ] }})
         .populate('user', 'firstName')
